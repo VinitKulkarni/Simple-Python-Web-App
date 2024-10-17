a1492ce0e26ddfe55c3e0a0dd4c89222876ce75d
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2024-10-17 13:34:32 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -3,15 +3,15 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    username = request.form['username']
-    requests.post('http://backend:5000/add_user', json={'username': username})
-    return 'User added!'
+    if request.method == 'POST':
+        username = request.form['username']
+        requests.post('http://be:5000/add_user', json={'username': username})
+    
+    # Fetch updated user list
+    users = requests.get('http://be:5000/').json()
+    return render_template('index.html', users=users)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
